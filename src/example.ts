e793f56a9f8101fa6091475248cb7f76f06bd668
lorenzvanherwaarden <lorenz.van.herwaarden@gmail.com>
2022-08-06 23:47:55 +0200

    chore: add comments

--- src/example.ts
+++ src/example.ts
@@ -1,11 +1,13 @@
 import './components'
 import type DatetimeWebComponent from './components/DatetimeWebComponent'
 
+// Get DatetimeWebComponent
 const datetimeWebComponent = document.getElementsByTagName(
   'datetime-web-component'
 )[0] as DatetimeWebComponent
 const input = document.getElementById('input') as HTMLElement
 
+// Set refElement
 datetimeWebComponent.refElement = input
 
 // datetimeWebComponent.value = '2021-01-05 12:43:23'
