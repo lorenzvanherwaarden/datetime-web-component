0ce0f55682199a2faf1b1ae2716f779eae3e82f2
Lorenz van Herwaarden <63999112+lorenzvanherwaarden@users.noreply.github.com>
2023-01-08 01:15:40 +0000

    chore: remove vitest

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,38 +1,18 @@
-import { Browser, chromium, firefox, Page, webkit } from "playwright";
-import { afterAll, beforeAll, describe, it } from "vitest";
 import { test, expect } from '@playwright/test';
 
+test('has title', async ({ page }) => {
+  await page.goto('/');
 
-const browserTypes = process.env.ALL_BROWSERS
-  ? [chromium, firefox, webkit]
-  : [chromium];
+  // Expect a title "to contain" a substring.
+  await expect(page.getByTestId('input')).toHaveAttribute('id', 'input');
+});
 
-for (const browserType of browserTypes) {
-  describe(`browser:${browserType.name()}`, () => {
-    let browser: Browser;
-    let page: Page;
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-    beforeAll(async () => {
-      browser = await browserType.launch({ headless: true });
-      const page = await browser.newPage();
-      page.on("console", (msg) => console.log(msg.text()));
-      await page.goto("http://localhost:5173");
-    });
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-    afterAll(async () => {
-      browser?.close();
-    });
-    it("evaluate with vite module", async () => {
-      await expect(page.getByTestId('input')).toHaveText('aaa');
-      
-      // await page.locator("#ready").waitFor({ state: "attached" });
-      // await page.evaluate(() => {
-      //   return new Promise(async (r) => {
-      //     const mod = await new Function("return import('/mod.ts')")();
-      //     console.log("hello in eval", Object.keys(mod));
-      //     r(null);
-      //   });
-      // });
-    });
-  });
-}
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*intro/);
+// });
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,4 @@ export default defineConfig({
       output: {},
     },
   },
-  test: {
-    globals: true
-  }
 })
